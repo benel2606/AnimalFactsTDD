6a43befbe4c746a545cba775b3dcdd2a02f325fd
Ben Aharon <benel2606@gmail.com>
2019-03-22 17:45:23 +0200

    refactor after lint and change file name

--- animal_facts_tests.py
+++ animal_facts_tests.py
@@ -1,7 +1,8 @@
-from nose.tools import assert_true
 import requests
+from nose.tools import assert_true
 
-def  test_get_animal_facts():
+
+def test_get_animal_facts():
     response = requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat,dog,snail,horse&amount=500')
     assert_true(response.ok)
     if response.ok:
