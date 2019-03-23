c1b1bb487698aa1fc7162dd651ab4b56854b8767
Ben Aharon <benel2606@gmail.com>
2019-03-23 16:10:19 +0200

    fixing lint

--- animal_facts_tests.py
+++ animal_facts_tests.py
@@ -1,9 +1,17 @@
+"""
+tests
+"""
 import requests
 from nose.tools import assert_true # pylint: disable=import-error
 
 
+
 def test_get_animal_facts():
-    response = requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat,dog,snail,horse&amount=500')
+    """
+    :return:
+    """
+    url = 'https://cat-fact.herokuapp.com/facts/random?animal_type=cat,dog,snail,horse&amount=500'
+    response = requests.get(url)
     assert_true(response.ok)
     if response.ok:
         return response
