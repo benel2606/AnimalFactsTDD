6794532842a71a9e15927d14c3c20f5423f2e159
Ben Aharon <benel2606@gmail.com>
2019-03-23 17:24:36 +0200

    testing pipline

--- animal_facts_tests.py
+++ animal_facts_tests.py
@@ -13,6 +13,7 @@ def test_get_animal_facts():
     url = 'https://cat-fact.herokuapp.com/facts/random?animal_type=cat,dog,snail,horse&amount=500'
     response = requests.get(url)
     assert_true(response.ok)
+    print(response.json()[0]["text"])
     if response.ok:
         return response
 
