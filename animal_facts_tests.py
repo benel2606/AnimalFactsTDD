0991dd0971edd93726127f6143640579d9c8f496
Ben Aharon <benel2606@gmail.com>
2019-03-24 12:43:41 +0200

    install mock in heroku

--- animal_facts_tests.py
+++ animal_facts_tests.py
@@ -6,7 +6,7 @@ unit testing for the 2 features:
 from __future__ import print_function
 from unittest.mock import Mock, patch
 from nose.tools import assert_equal  # pylint: disable=import-error
-from animal_facts_function import get_animal_facts
+from animal_facts_function import get_animal_facts,get_facts_by_word
 from animal_facts_mock_dictionary import facts
 
 
